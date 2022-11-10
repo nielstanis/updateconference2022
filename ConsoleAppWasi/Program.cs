4407a3d81e013873a94a55d40442846c6dbc986d
Niels Tanis <nielstanis@live.nl>
2022-11-10 10:34:13 +0100

    Initial material

---
+++ ConsoleAppWasi/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Runtime.InteropServices;
+
+Console.WriteLine($"Hello from {RuntimeInformation.OSArchitecture}!");
+
+var file = System.IO.File.ReadAllText("/etc/hosts");
+Console.WriteLine(file);
---
+++ Extend/ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Hello, UpdateConference 2022!");
+
+Directory.CreateDirectory("log");
+Wasmtime.WasiConfiguration confi = new Wasmtime.WasiConfiguration()
+    .WithStandardError("log/error.log")
+    .WithStandardOutput("log/output.log")
+    .WithArg("CalledFromConsole")
+    .WithArgs(args); //passing args
+
+var engine = new Wasmtime.Engine();
+var linker = new Wasmtime.Linker(engine);
+linker.DefineWasi();
+var store = new Wasmtime.Store(engine);
+store.SetWasiConfiguration(confi);
+
+string wasm = @"/Users/ntanis/GitHub/updateconference2022/Extend/MyRustModule/target/wasm32-wasi/debug/myrustmodule.wasm";
+var module = Wasmtime.Module.FromFile(engine, wasm);
+var inst = linker.Instantiate(store, module);
+var main = inst.GetFunction("main");
+
+if (main!=null)
+    main.Invoke(0,0);
